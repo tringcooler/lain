76b28555ca0882078e28d274b35e886e6e920126
tringcooler <tringcooler@users.noreply.github.com>
2018-02-18 18:34:50 +0800

    standard commit
    永远新建cluster，不使用lazy方式存储，以便直接用于add中存放结果

--- src/core/link.py
+++ src/core/link.py
@@ -332,13 +332,9 @@ class _lain_chain(object):
     def trivial(self):
         return self
 
-    @lazypropdh
+    @property
     def cluster(self):
-        self.links #refresh chain links dirty flag
         return self.split(self.meta)
-
-    def cluster_dirty(self):
-        return self.links_dirty()
 
     def get_links(self, root_out = False):
         rs = self.links.copy()
